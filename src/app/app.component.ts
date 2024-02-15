3ea6c5186f6d5384a34d4908d368c28b22a6ac08
vsrivatsan <vsrivatsan@vanenburg.com>
2024-02-15 11:47:45 +0530

    - Basic working website v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,281 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+declare var Typed: any,
+  GLightbox: any,
+  PureCounter: any,
+  AOS: any,
+  Waypoint: any,
+  Swiper: any,
+  Isotope: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-personal-portfolio';
+export class AppComponent implements AfterViewInit {
+  public experiences: any[];
+  public certificates: {image: any, document: any}[] = [];
+  public skills: any[];
+
+  public colorToClass = {
+    "red": "bg-danger",
+    "blue": "bg-info",
+    "green": "bg-success",
+    "dark-blue": "bg-primary",
+    "yellow": "bg-warning"
+  }
+
+  constructor(
+    httpClient: HttpClient,
+    sanitizer: DomSanitizer
+    ) {
+    httpClient.get('assets/data/info.json').subscribe(
+      (data: any) => {
+        this.experiences = data.experiences;
+        this.skills = data.skills;
+
+        data.certificates.forEach(
+          certificate => this.certificates.push({
+            "image": certificate + ".jpg",
+            "document": sanitizer.bypassSecurityTrustResourceUrl(certificate + ".pdf")
+          })
+        );
+
+        this.skills.sort((a,b) => parseInt(b.percent) - parseInt(a.percent));
+
+        setTimeout(this.initCertificateLightbox.bind(this), 1000);
+
+        console.log(data)
+      }
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.processJavascriptFunctions();
+  }
+
+  processJavascriptFunctions() {
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim();
+      if (all) {
+        // @ts-ignore
+        return [...document.querySelectorAll(el)];
+      } else {
+        return document.querySelector(el);
+      }
+    };
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all);
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach((e) => e.addEventListener(type, listener));
+        } else {
+          selectEl.addEventListener(type, listener);
+        }
+      }
+    };
+
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener);
+    };
+
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true);
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200;
+      navbarlinks.forEach((navbarlink) => {
+        if (!navbarlink.hash) return;
+        let section = select(navbarlink.hash);
+        if (!section) return;
+        if (
+          position >= section.offsetTop &&
+          position <= section.offsetTop + section.offsetHeight
+        ) {
+          navbarlink.classList.add('active');
+        } else {
+          navbarlink.classList.remove('active');
+        }
+      });
+    };
+    window.addEventListener('load', navbarlinksActive);
+    onscroll(document, navbarlinksActive);
+
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+      let elementPos = select(el).offsetTop;
+      window.scrollTo({
+        top: elementPos,
+        behavior: 'smooth',
+      });
+    };
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top');
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active');
+        } else {
+          backtotop.classList.remove('active');
+        }
+      };
+      window.addEventListener('load', toggleBacktotop);
+      onscroll(document, toggleBacktotop);
+    }
+
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function (e) {
+      select('body').classList.toggle('mobile-nav-active');
+      this.classList.toggle('bi-list');
+      this.classList.toggle('bi-x');
+    });
+
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on(
+      'click',
+      '.scrollto',
+      function (e) {
+        if (select(this.hash)) {
+          e.preventDefault();
+
+          let body = select('body');
+          if (body.classList.contains('mobile-nav-active')) {
+            body.classList.remove('mobile-nav-active');
+            let navbarToggle = select('.mobile-nav-toggle');
+            navbarToggle.classList.toggle('bi-list');
+            navbarToggle.classList.toggle('bi-x');
+          }
+          scrollto(this.hash);
+        }
+      },
+      true
+    );
+
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
+    window.addEventListener('load', () => {
+      if (window.location.hash) {
+        if (select(window.location.hash)) {
+          scrollto(window.location.hash);
+        }
+      }
+    });
+
+    /**
+     * Hero type effect
+     */
+    const typed = select('.typed');
+    if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items');
+      typed_strings = typed_strings.split(',');
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000,
+      });
+    }
+
+    /**
+     * Skills animation
+     */
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+      new Waypoint({
+        element: skilsContent,
+        offset: '80%',
+        handler: function (direction) {
+          let progress = select('.progress .progress-bar', true);
+          progress.forEach((el) => {
+            el.style.width = el.getAttribute('aria-valuenow') + '%';
+          });
+        },
+      });
+    }
+
+    /**
+     * Porfolio isotope and filter
+     */
+    window.addEventListener('load', () => {
+      let certificateContainer = select('.certificate-container');
+      if (certificateContainer) {
+        let certificateIsotope = new Isotope(certificateContainer, {
+          itemSelector: '.certificate-item',
+        });
+
+        let certificateFilters = select('#certificate-flters li', true);
+
+        on(
+          'click',
+          '#certificate-flters li',
+          function (e) {
+            e.preventDefault();
+            certificateFilters.forEach(function (el) {
+              el.classList.remove('filter-active');
+            });
+            this.classList.add('filter-active');
+
+            certificateIsotope.arrange({
+              filter: this.getAttribute('data-filter'),
+            });
+            certificateIsotope.on('arrangeComplete', function () {
+              AOS.refresh();
+            });
+          },
+          true
+        );
+      }
+    });
+
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false,
+      });
+    });
+
+    /**
+     * Initiate Pure Counter
+     */
+    new PureCounter();
+  }
+
+  initCertificateLightbox() {
+        /**
+     * Initiate certificate lightbox
+     */
+      const certificateLightbox = GLightbox({
+        selector: '.certificate-lightbox',
+      });
+  }
 }
